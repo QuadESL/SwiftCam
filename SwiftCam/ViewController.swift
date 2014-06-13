19a88c2509e60fe1f6f081cb6570d8f8beb2cf17
田中 賢治 <key.m4096.kung.fu@gmail.com>
2014-06-13 23:33:51 +0900

    写真を撮ってカメラロールに保存するところまで出来た

--- SwiftCam/ViewController.swift
+++ SwiftCam/ViewController.swift
@@ -9,47 +9,43 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate
+{
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-        
+    
+    //UIImagePickerControllerはviewDidLoadの中では書けないらしい
     override func viewDidAppear(animated: Bool) {
-        //カメラが使えるかチェックして、使えたらカメラを起動
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-            println("カメラ取得")
-            let imagePicker:UIImagePickerController = UIImagePickerController()
+            var imagePicker:UIImagePickerController = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
             imagePicker.allowsEditing = false
             self.presentViewController(imagePicker, animated: true, completion: nil)
         }
+    }
     
-        //画像が選択された時に呼ばれる
-        func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
-            println("画像選択完了")
-            
-            
-            self.dismissModalViewControllerAnimated(true)
-        }
-        
-        func UIImageWriteToSavedPhotosAlbum(image: UIImage!, completionTarget: AnyObject!, completionSelector: Selector, contextInfo: CMutableVoidPointer){
-            if completionTarget {
-                // 保存失敗時
-                println("保存失敗")
-            }else{
-                // 保存成功時
-                println("保存成功")
-            }
-        }
-        
-        //画像の選択がキャンセルされた時に呼ばれる
-        func imagePickerControllerDidCancel(picker: UIImagePickerController!){
-            self.dismissModalViewControllerAnimated(true)
+    //UIImagePickerControllerのデリゲートメソッド
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        UIImageWriteToSavedPhotosAlbum(image, self, "imageDidFinishSavingWithErrorContextInfo:error:contextInfo:", nil)
+        self.dismissModalViewControllerAnimated(true)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController!) -> Void {
+        NSLog("Canceled")
+        self.dismissModalViewControllerAnimated(true)
+    }
+    
+    //UIImageWriteToSavedPhotosAlbumのcompletionメソッド
+    func imageDidFinishSavingWithErrorContextInfo(image: UIImage!, error: NSErrorPointer, contextInfo: CMutableVoidPointer){
+        if error {
+            NSLog("ImageSaving Failed")
+        }else{
+            NSLog("ImageSaving Succeeded")
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
